ee0a41d6d6c0f9446c2465419241c0cceebf644d
snarlynarwhal <pjlegendreiv@gmail.com>
2024-04-26 15:13:25 -0400

    Added support for aggregating conditions

--- Runtime/Scripts/ConditionAsset.cs
+++ Runtime/Scripts/ConditionAsset.cs
@@ -1,11 +1,14 @@
 using HHG.Common.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using UnityEngine;
 
 namespace HHG.StatSystem.Runtime
 {
     [CreateAssetMenu(fileName = "Condition", menuName = "HHG/Stat System/Condition")]
-    public class ConditionAsset : ScriptableObject
+    public class ConditionAsset : ScriptableObject, IAggregatable<ConditionAsset>
     {
         public string Tag
         {
@@ -20,14 +23,14 @@ namespace HHG.StatSystem.Runtime
         }
         public float Priority => priority;
         public float Duration => duration;
-        public MetaBehaviour[] Behaviours => behaviours;
+        public IReadOnlyList<MetaBehaviour> Behaviours => behaviours;
 
         [SerializeField] private string tag;
         [SerializeField] private float priority;
         [SerializeField] private float duration;
-        [SerializeField] private StatsMod[] mods;
+        [SerializeField] private List<StatsMod> mods = new List<StatsMod>();
 
-        [SerializeReference, SerializeReferenceDropdown] private MetaBehaviour[] behaviours;
+        [SerializeReference, SerializeReferenceDropdown] private List<MetaBehaviour> behaviours = new List<MetaBehaviour>();
 
         public void Apply(GameObject target)
         {
@@ -43,6 +46,32 @@ namespace HHG.StatSystem.Runtime
             {
                 tracker.Remove(this);
             }
+        }
+
+        public ConditionAsset Aggregate(ConditionAsset other)
+        {
+            Dictionary<Type, MetaBehaviour> dict = new Dictionary<Type, MetaBehaviour>();
+            mods.AddRange(other.mods);
+            // IEnumerables reference source collections, so convert to a list
+            // to make sure that when we clear behaviours, it doesn't affect combined
+            List<MetaBehaviour> combined = behaviours.Concat(other.behaviours).ToList();
+            behaviours.Clear(); // Clear after get combined
+            
+            foreach (MetaBehaviour behaviour in combined)
+            {
+                if (behaviour is IAggregatable)
+                {
+                    Type type = behaviour.GetType();
+                    dict[type] = dict.ContainsKey(type) ? (MetaBehaviour)((IAggregatable)dict[type]).Aggregate(behaviour) : behaviour;
+                }
+                else
+                {
+                    behaviours.Add(behaviour);
+                }
+            }
+
+            behaviours.AddRange(dict.Values);
+            return this;
         }
 
         private void OnValidate()
--- Runtime/Scripts/ConditionAssetExtensions.cs
+++ Runtime/Scripts/ConditionAssetExtensions.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace HHG.StatSystem.Runtime
@@ -18,6 +19,29 @@ namespace HHG.StatSystem.Runtime
             foreach (ConditionAsset condition in conditions)
             {
                 condition.Remove(target);
+            }
+        }
+
+        public static void AggregateTo(this IEnumerable<ConditionAsset> source, List<ConditionAsset> aggregated)
+        {
+            foreach (ConditionAsset item in aggregated)
+            {
+                Object.Destroy(item);
+            }
+
+            aggregated.Clear();
+
+            if (source.Any())
+            {
+                Dictionary<string, ConditionAsset> dict = new Dictionary<string, ConditionAsset>();
+
+                foreach (ConditionAsset condition in source)
+                {
+                    string tag = condition.Tag;
+                    dict[tag] = dict.ContainsKey(tag) ? dict[tag].Aggregate(condition) : Object.Instantiate(condition);
+                }
+
+                aggregated.AddRange(dict.Values);
             }
         }
     }
